8b76cc62937582fee62f0193e2edf586d6382921
Alvarez Gustavo <alvarezgustavomatias@gmail.com>
2023-05-17 20:30:58 -0300

    agregados los 5 tests

--- src/test/java/com/hits/HiberusTest/controller/TestController.java
+++ src/test/java/com/hits/HiberusTest/controller/TestController.java
@@ -17,10 +17,82 @@ public class TestController {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    public void CUANDO_se_llama_a_usuarios_ENTONCES_el_estado_es_200() throws Exception {
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/v1/price").param("","")
+    @Test
+    public void test1() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/price")
+                        .param("productId","35455")
+                        .param("brandId","1")
+                        .param("date","2020-06-14 10:00:00")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .isOk())
+                .andExpect(MockMvcResultMatchers
+                        .content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+
+    @Test
+    public void test2() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/price")
+                        .param("productId","35455")
+                        .param("brandId","1")
+                        .param("date","2020-06-14 16:00:00")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .isOk())
+                .andExpect(MockMvcResultMatchers
+                        .content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+
+    @Test
+    public void test3() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/price")
+                        .param("productId","35455")
+                        .param("brandId","1")
+                        .param("date","2020-06-14 21:00:00")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .isOk())
+                .andExpect(MockMvcResultMatchers
+                        .content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+
+    @Test
+    public void test4() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/price")
+                        .param("productId","35455")
+                        .param("brandId","1")
+                        .param("date","2020-06-15 10:00:00")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .isOk())
+                .andExpect(MockMvcResultMatchers
+                        .content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+
+    @Test
+    public void test5() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/price")
+                        .param("productId","35455")
+                        .param("brandId","1")
+                        .param("date","2020-06-16 21:00:00")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers
                         .status()
